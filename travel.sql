1e2369cbe8816d264d799e86b397426c536aae47
Vivek Kumar <vivek.kumar@consultadd.com>
2024-01-19 22:43:12 +0530

    User Table Created

--- travel.sql
+++ travel.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jan 19, 2024 at 05:25 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
