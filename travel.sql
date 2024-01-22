53f145c39e59191ff25e1bc489b353dea05b7df1
Yashraj Singh Chouhan <yashrajsingh@Yashrajs-MacBook-Pro.local>
2024-01-22 17:26:56 +0530

    simplifying and adding some attributes like phonenumber set role default NORMAL destination attribute is broken into title and location travel_id set none unique so that in booking different user can book same plan

--- travel.sql
+++ travel.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 20, 2024 at 06:25 PM
+-- Generation Time: Jan 22, 2024 at 12:53 PM
 -- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,159 +24,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `booking`
---
-
-CREATE TABLE `booking` (
-  `booking_id` int(11) NOT NULL,
-  `booking_price` int(11) NOT NULL,
-  `travel_plan_travel_id` int(11) NOT NULL,
-  `user_user_id` int(11) NOT NULL,
-  `member_count` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `booking`
---
-
-INSERT INTO `booking` (`booking_id`, `booking_price`, `travel_plan_travel_id`, `user_user_id`, `member_count`) VALUES
-(2, 1800, 3, 3, 3),
-(4, 1000, 2, 5, 2),
-(8, 3000, 4, 5, 3);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `booking_seq`
---
-
-CREATE TABLE `booking_seq` (
-  `next_val` bigint(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `booking_seq`
---
-
-INSERT INTO `booking_seq` (`next_val`) VALUES
-(1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `travel_plan`
---
-
-CREATE TABLE `travel_plan` (
-  `travel_id` int(11) NOT NULL,
-  `destination` varchar(255) NOT NULL,
-  `rate` int(11) NOT NULL,
-  `start_date` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `travel_plan`
---
-
-INSERT INTO `travel_plan` (`travel_id`, `destination`, `rate`, `start_date`) VALUES
-(2, 'Haridwar', 500, '25/01/2024'),
-(3, 'Kashi', 600, '01/02/2024'),
-(4, 'Ayodhya', 1000, '22/01/2024');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `travel_plan_seq`
---
-
-CREATE TABLE `travel_plan_seq` (
-  `next_val` bigint(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `travel_plan_seq`
---
-
-INSERT INTO `travel_plan_seq` (`next_val`) VALUES
-(1);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `phonenumber` varchar(20) NOT NULL,
   `name` varchar(20) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `role` varchar(255) NOT NULL
+  `role` varchar(255) NOT NULL DEFAULT 'NORMAL'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `email`, `name`, `password`, `role`) VALUES
-(2, 'sita@gmail.com', 'Sita Kumari', 'Sita', 'ADMIN'),
-(3, 'shyam@gmail.com', 'Shyam Kumar', 'Shyam', 'NORMAL'),
-(4, 'geeta@gmail.com', 'Geeta Kumari', 'Geeta', 'NORMAL'),
-(5, 'mira@gmail.com', 'Mira Kumari', 'Mira', 'NORMAL'),
-(6, 'ram@gmail.com', 'Ram Kumar', 'Ram', 'ADMIN');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_seq`
---
-
-CREATE TABLE `user_seq` (
-  `next_val` bigint(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `user_seq`
---
-
-INSERT INTO `user_seq` (`next_val`) VALUES
-(1);
+INSERT INTO `user` (`user_id`, `email`, `phonenumber`, `name`, `password`, `role`) VALUES
+(2, 'sita@gmail.com', '1234567789', 'Sita Kumari', 'Sita', 'ADMIN'),
+(3, 'shyam@gmail.com', '2345675436', 'Shyam Kumar', 'Shyam', 'NORMAL'),
+(4, 'geeta@gmail.com', '87687686456', 'Geeta Kumari', 'Geeta', 'NORMAL'),
+(5, 'mira@gmail.com', '23454565678', 'Mira Kumari', 'Mira', 'NORMAL'),
+(6, 'ram@gmail.com', '8678665564', 'Ram Kumar', 'Ram', 'ADMIN');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `booking`
---
-ALTER TABLE `booking`
-  ADD PRIMARY KEY (`booking_id`),
-  ADD UNIQUE KEY `UK_arhqgchhkljgxpc73gc9lppwa` (`travel_plan_travel_id`),
-  ADD KEY `FKdxt6l0m3hxp1frtqkkjxd1isj` (`user_user_id`);
-
---
--- Indexes for table `travel_plan`
---
-ALTER TABLE `travel_plan`
-  ADD PRIMARY KEY (`travel_id`);
-
---
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `UK_ob8kqyqqgmefl0aco34akdtpe` (`email`);
+  ADD PRIMARY KEY (`user_id`);
 
 --
--- Constraints for dumped tables
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- Constraints for table `booking`
+-- AUTO_INCREMENT for table `user`
 --
-ALTER TABLE `booking`
-  ADD CONSTRAINT `FKdxt6l0m3hxp1frtqkkjxd1isj` FOREIGN KEY (`user_user_id`) REFERENCES `user` (`user_id`),
-  ADD CONSTRAINT `FKetlnww9bc1vpw3yljagn75a4u` FOREIGN KEY (`travel_plan_travel_id`) REFERENCES `travel_plan` (`travel_id`);
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
